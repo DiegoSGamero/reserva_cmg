52acaae8f292dee44c69cab33ee30f571421c2d2
diego joao <diegosgjoao@gmail.com>
2024-10-31 20:42:17 -0300

    trying to create an event to display the comfirm message popup

---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="popoup"
+export default class extends Controller {
+  connect() {
+    console.log("Hello from popup_controller.js")
+  }
+}
